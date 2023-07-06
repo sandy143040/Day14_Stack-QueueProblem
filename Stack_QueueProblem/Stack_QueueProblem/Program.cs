806c4eb427605d62a75f034bcc0bb023dd93dbed
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-07-07 00:36:56 +0530

    [Sandeep] add : create a stack using generics

---
+++ Stack_QueueProblem/Stack_QueueProblem/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_QueueProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StackOperations<int> stackOperations = new StackOperations<int>();
+            stackOperations.Push(70);
+            stackOperations.Push(30);
+            stackOperations.Push(56);
+            stackOperations.Display();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Stack_QueueProblem/Stack_QueueProblem/StackOperations.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_QueueProblem
+{
+    public class StackOperations<T>
+    {
+        public Node<T> top;
+        public void Push(T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+            if(top == null)
+            {
+                top = newNode;
+            }
+            else
+            {
+                newNode.next = top;
+                top = newNode;
+            }
+        }
+        public void Display()
+        {
+            Node<T> temp = top;
+            if(top == null)
+            {
+                Console.WriteLine("Stack is Empty");
+            }
+            while(temp != null)
+            {
+                Console.WriteLine(temp.data);
+                temp = temp.next;
+            }
+        }
+    }
+}
